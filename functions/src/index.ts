f3b978ff8ae44febdcdecff30a7a5e2c58efeb09
Sergey Okhotnikov <ognerezov@gmail.com>
2020-04-09 17:49:42 +0300

    extra bug handling

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -20,26 +20,34 @@ export const getChapters = functions.region('europe-west3')
             if(request.method ==='OPTIONS'){
                 response.sendStatus(204);
             }
-
-            const password = (await passwordDao.get()).data();
-            if(password){
-                /*
-                    I want one secret temporal password valid for any user
-                 */
-                if(password.password !== request.body.password){
-                    response.sendStatus(401);
+            const res : any[] =[];
+            try {
+                const password = (await passwordDao.get()).data();
+                if(password){
+                    /*
+                        I want one secret temporal password valid for any user
+                     */
+                    if(password.password !== request.body.password){
+                        response.sendStatus(401);
+                    }
                 }
+
+                let numbers : number[] = request.body.numbers;
+                if(!numbers || numbers.length ===0){
+                    numbers = [1];
+                }
+
+
+                const snap = await bookDao
+                    .where('number','in',numbers)
+                    .get();
+                snap.forEach(
+                    doc=>res.push(doc.data())
+                );
+            }catch (e) {
+                response.status(500).send(e);
             }
 
-            const numbers : number[] = request.body.numbers;
-            const snap = await bookDao
-                .where('number','in',numbers)
-                .get();
-
-            const res : any[] =[];
-            snap.forEach(
-                doc=>res.push(doc.data())
-            );
             response.send(res);
         })
     });
